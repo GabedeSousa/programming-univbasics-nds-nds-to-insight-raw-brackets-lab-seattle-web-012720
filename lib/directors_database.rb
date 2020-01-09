91f2b8c59b46b54bec7bdb5e7fcb6c0f1993b744
Gabriel Fidelis de Sousa <gabriel.fidelis.sousa@gmail.com>
2020-01-09 19:49:20 +0000

    Automatically backed up by Learn

--- lib/directors_database.rb
+++ lib/directors_database.rb
@@ -5,11 +5,16 @@ def directors_database
   @_db ||= YAML.load(rot13.call(File.open("directors_db").read.to_s))
 end
 
-def directors_totals(source)
-  result = {}
-  director_index = 0 
-  result 
-end
+def directors_totals(nds)
+   result = {}		   result = {}
+   nil		   i = 0
+   while i < nds.size do
+     director = nds[i]
+     result[director[:name]] = gross_for_director(director)
+     i += 1
+   end
+   result
+ end		
 
  
 
