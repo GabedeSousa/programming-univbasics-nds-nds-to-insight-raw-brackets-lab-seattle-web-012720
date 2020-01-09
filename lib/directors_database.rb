93b1fe1cdc9d375080eafd00aa3cc5f9b59f7202
Gabriel Fidelis de Sousa <gabriel.fidelis.sousa@gmail.com>
2020-01-09 19:43:49 +0000

    Automatically backed up by Learn

--- lib/directors_database.rb
+++ lib/directors_database.rb
@@ -5,13 +5,7 @@ def directors_database
   @_db ||= YAML.load(rot13.call(File.open("directors_db").read.to_s))
 end
 
-def print_first_directors_movie_titles
-   index = 0
-   while index < directors_database[0][:movies].length do
-     puts directors_database[0][:movies][index][:title]
-     index += 1
-   end
- end		
+
  
 
 
